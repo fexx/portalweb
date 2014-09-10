3212ff5f71464f590e9ff00691557f47094676a0
Fernando <Fernando@10.0.108.57>
2014-09-10 08:22:06 -0300

    #3 - Atualizar versão do spring com o java8 e o wildfly 8

--- src/main/java/br/com/portalweb/controller/LoginController.java
+++ src/main/java/br/com/portalweb/controller/LoginController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.com.portalweb.dao.UsuarioDAO;
 import br.com.portalweb.exception.DAOException;
 import br.com.portalweb.model.Usuario;
-import br.com.portalweb.utils.SHA;
 
 @Controller
 @RequestMapping("/login")
@@ -19,23 +18,23 @@ public class LoginController {
 	@Autowired
 	private UsuarioDAO usuarioDAO;
 	
-	@Autowired
-	private SHA sha;
+//	@Autowired
+//	private SHA sha;
 	
 	
 	@RequestMapping("/login/usuario")
 	public void usuario(Usuario usuario) throws NoResultException, IllegalArgumentException, AuthenticationException, DAOException{
-		Usuario usuarioBD = usuarioDAO.buscaPorEmail(usuario.getEmail());
+//		Usuario usuarioBD = usuarioDAO.buscaPorEmail(usuario.getEmail());
 		
 		
-		if(usuarioBD != null){
-				if(sha.matches(usuarioBD.getSenha(), usuario.getSenha())){
-					
-				}
-			
-		}else{
-			
-		}
+//		if(usuarioBD != null){
+//				if(sha.matches(usuarioBD.getSenha(), usuario.getSenha())){
+//					
+//				}
+//			
+//		}else{
+//			
+//		}
 	}
 	
 
